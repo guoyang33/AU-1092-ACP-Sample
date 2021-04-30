9816fca82c0b3bd1cf024e5293e27ad4762bb4f4
guoyang33@I628 <guoyang33@gmail.com>
2021-04-30 15:28:55 +0800

    Week10

---
+++ Week10/Q4.py
@@ -0,0 +1,18 @@
+import sys
+
+if __name__=='__main__':
+    if len(sys.argv)>1:
+        text = sys.argv[1]
+        wordDict = {}
+        text.replace('.', '')
+        text.replace(',', '')
+        text.replace('\t', '')
+        text.replace('\n', '')
+        for word in text.split():
+            if word not in wordDict:
+                wordDict[word] = 1
+            else:
+                wordDict[word] = wordDict[word] + 1
+
+        for i in wordDict:
+            print('{0}  =>  {1}'.format(i, wordDict[i]))
